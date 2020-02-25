e90c3c043c43ce11ecf8ffb8e462fee82e0d5c9f
AIT-RAMI <mohamedaitrami@gmail.com>
2020-02-25 14:48:58 +0100

    Adding empty project

---
+++ gradlew.bat
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <item name="android.os.AsyncTask Result doInBackground(Params...)">
+    <annotation name="android.support.annotation.WorkerThread" />
+  </item>
+  <item name="android.os.AsyncTask android.os.AsyncTask&lt;Params,Progress,Result&gt; execute(Params...)">
+    <annotation name="android.support.annotation.MainThread" />
+  </item>
+  <item name="android.os.AsyncTask android.os.AsyncTask&lt;Params,Progress,Result&gt; executeOnExecutor(java.util.concurrent.Executor, Params...)">
+    <annotation name="android.support.annotation.MainThread" />
+  </item>
+  <item name="android.os.AsyncTask void execute(java.lang.Runnable)">
+    <annotation name="android.support.annotation.MainThread" />
+  </item>
+  <item name="android.os.AsyncTask void onCancelled()">
+    <annotation name="android.support.annotation.MainThread" />
+  </item>
+  <item name="android.os.AsyncTask void onCancelled(Result)">
+    <annotation name="android.support.annotation.MainThread" />
+  </item>
+  <item name="android.os.AsyncTask void onPostExecute(Result)">
+    <annotation name="android.support.annotation.MainThread" />
+  </item>
+  <item name="android.os.AsyncTask void onPreExecute()">
+    <annotation name="android.support.annotation.MainThread" />
+  </item>
+  <item name="android.os.AsyncTask void onProgressUpdate(Progress...)">
+    <annotation name="android.support.annotation.MainThread" />
+  </item>
+  <item name="android.os.AsyncTask void publishProgress(Progress...)">
+    <annotation name="android.support.annotation.WorkerThread" />
+  </item>
+  <item name="android.os.BaseBundle boolean[] getBooleanArray(java.lang.String)">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.os.BaseBundle boolean[] getBooleanArray(java.lang.String) 0">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.os.BaseBundle double[] getDoubleArray(java.lang.String)">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.os.BaseBundle double[] getDoubleArray(java.lang.String) 0">
+    <annotation name="android.support.annotation.Nullable" />
+  </item>
+  <item name="android.os.BaseBundle int[] getIntArray(java.lang.String)">
+   
