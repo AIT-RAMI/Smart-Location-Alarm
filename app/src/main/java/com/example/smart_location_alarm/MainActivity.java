cfb1255411b856e05c2bcddf0d90c265ecc6f53b
lionel <lionelvb0@gmail.com>
2020-02-25 14:53:55 +0100

    Adding a comment

--- app/src/main/java/com/example/smart_location_alarm/MainActivity.java
+++ app/src/main/java/com/example/smart_location_alarm/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.smart_location_alarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+// This is a comment
 public class MainActivity extends AppCompatActivity {
 
     @Override
