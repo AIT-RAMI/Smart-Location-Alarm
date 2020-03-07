dc860afedd8a8a4ebbd33f344f2a1f4eab8f9e39
ABED Abir <57662791+ABEDabir@users.noreply.github.com>
2020-03-07 12:13:55 +0100

    adding splash screen

--- app/src/main/java/com/example/smart_location_alarm/MainActivity.java
+++ app/src/main/java/com/example/smart_location_alarm/MainActivity.java
@@ -1,14 +1,23 @@
-package com.example.smart_location_alarm;
+package com.example.alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-// This is a comment
-public class MainActivity extends AppCompatActivity {
+import android.os.Handler;
 
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+        public void run(){
+            Intent homeIntent=new Intent(MainActivity.this,Welcome_Activity.class);
+            startActivity(homeIntent);
+        }
+    },SPLASH_TIME_OUT);
     }
 }
---
+++ app/src/main/java/com/example/smart_location_alarm/Welcome_Activity.java
@@ -0,0 +1,16 @@
+package com.example.alarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Welcome_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_);
+
+    }
+}
