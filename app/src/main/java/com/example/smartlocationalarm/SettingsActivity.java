f9a26fcd4d1afa7d8a23ec3ef7d83133e49ff89a
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-31 17:15:06 +0100

    seekbar config

--- app/src/main/java/com/example/smartlocationalarm/SettingsActivity.java
+++ app/src/main/java/com/example/smartlocationalarm/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.example.smartlocationalarm;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
@@ -12,6 +15,7 @@ public class SettingsActivity extends AppCompatActivity {
     SwitchCompat s1, s2;
     boolean state1, state2;
     SharedPreferences pref;
+    SeekBar volControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +52,27 @@ public class SettingsActivity extends AppCompatActivity {
                 SharedPreferences.Editor ed = pref.edit();
                 ed.putBoolean("s2", state2);
                 ed.apply();
+            }
+        });
+
+        final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        volControl = findViewById(R.id.seekbar);
+        volControl.setMax(maxVolume);
+        volControl.setProgress(curVolume);
+        volControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, arg1, 0);
             }
         });
     }
