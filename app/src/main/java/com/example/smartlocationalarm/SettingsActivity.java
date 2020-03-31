d0ebf2c1d8bae87addf27c2ce8286404ec70c1f9
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-31 18:58:30 +0100

    vibrate mode done

--- app/src/main/java/com/example/smartlocationalarm/SettingsActivity.java
+++ app/src/main/java/com/example/smartlocationalarm/SettingsActivity.java
@@ -5,16 +5,24 @@ import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    static final String[] metrics = {"Metric System(m/km)", "Imperial System(ft/mi)"};
+    Spinner spi;
 
     SwitchCompat s1, s2;
     boolean state1, state2;
     SharedPreferences pref;
+
     SeekBar volControl;
 
     @Override
@@ -22,6 +30,34 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        spi = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(SettingsActivity.this,
+                android.R.layout.simple_spinner_item, metrics);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spi.setAdapter(adapter);
+        spi.setOnItemSelectedListener(this);
+
+        final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        volControl = findViewById(R.id.seekbar);
+        volControl.setMax(maxVolume);
+        volControl.setProgress(curVolume);
+        volControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, arg1, 0);
+            }
+        });
 
         pref = getSharedPreferences("PREFS", 0);
         state1 = pref.getBoolean("s1", false);
@@ -45,8 +81,14 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
         s2.setOnClickListener(new View.OnClickListener() {
+            final AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
             @Override
             public void onClick(View v) {
+                if (am.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE) {
+                    am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                } else {
+                    am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                }
                 state2 = !state2;
                 s2.setChecked(state2);
                 SharedPreferences.Editor ed = pref.edit();
@@ -55,25 +97,25 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        volControl = findViewById(R.id.seekbar);
-        volControl.setMax(maxVolume);
-        volControl.setProgress(curVolume);
-        volControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onStopTrackingTouch(SeekBar arg0) {
-            }
+    }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar arg0) {
-            }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                Toast.makeText(this, metrics[0],
+                        Toast.LENGTH_LONG).show();
+                break;
 
-            @Override
-            public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, arg1, 0);
-            }
-        });
+            case 1:
+                Toast.makeText(this, metrics[1],
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        return;
     }
 }
