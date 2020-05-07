d7d6acdc2e66c11b4ae4e3ee1fa1ae533bd0199d
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-05-07 17:48:09 +0000

    notification cleaning

--- app/src/main/java/com/example/smartlocationalarm/Welcome_Activity.java
+++ app/src/main/java/com/example/smartlocationalarm/Welcome_Activity.java
@@ -1,17 +1,11 @@
 package com.example.smartlocationalarm;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 
 public class Welcome_Activity extends AppCompatActivity {
 
@@ -66,8 +60,6 @@ public class Welcome_Activity extends AppCompatActivity {
             }
         });
 
-
-
     }
 
     private void existing() {
@@ -78,37 +70,6 @@ public class Welcome_Activity extends AppCompatActivity {
     private void clickCreate(){
         Intent intent = new Intent(this, PermissionActivity.class);
         this.startActivity(intent);
-    }
-
-    private void addNotification() {
-        String notif_text = "Vous avez programmé une alarme dans cette région! Rentrez voir de quoi s'agit il.";
-        String notif_title = "Nouvelle Notification";
-
-        Intent intent = new Intent(this, NotificationActivity.class);
-        intent.putExtra("text", notif_text);
-
-        PendingIntent pdIntent = PendingIntent.getActivity(Welcome_Activity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.alarm)
-                .setContentTitle(notif_title)
-                .setContentText(notif_text)
-                .setContentIntent(pdIntent)
-                .setAutoCancel(true);
-
-        NotificationManager ntfManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelId = "5";
-            NotificationChannel channel = new NotificationChannel(
-                    channelId,
-                    "Ceci est le titre pour la channel",
-                    NotificationManager.IMPORTANCE_HIGH);
-            ntfManager.createNotificationChannel(channel);
-            builder.setChannelId(channelId);
-        }
-
-        ntfManager.notify(0, builder.build());
     }
 
     public void AllAlarms(View view) {
