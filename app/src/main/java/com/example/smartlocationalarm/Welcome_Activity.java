be35096e35259f33a7ac39f3d8853afc43f35c53
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-05-08 07:29:03 +0000

    drawer finished

--- app/src/main/java/com/example/smartlocationalarm/Welcome_Activity.java
+++ app/src/main/java/com/example/smartlocationalarm/Welcome_Activity.java
@@ -2,61 +2,63 @@ package com.example.smartlocationalarm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Welcome_Activity extends AppCompatActivity {
 
-    Button create;
-    Button exist;
-    Button nav;
-    Button notif;
-    Button allalarms;
-    Button data;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView nv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_);
 
-        notif = findViewById(R.id.settings);
-        notif.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Welcome_Activity.this, WelcomeActivity2.class);
-                startActivity(intent);
-            }
-        });
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
 
-        create = findViewById(R.id.create);
-        create.setOnClickListener(new View.OnClickListener(){
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nv = findViewById(R.id.navView);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                clickCreate();
-            }
-        });
-        exist = findViewById(R.id.exist);
-        nav = findViewById(R.id.nav);
-        nav.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Welcome_Activity.this, Main2Activity.class);
-                startActivity(intent);
-            }
-        });
-        exist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                existing();
-            }
-        });
-        allalarms = findViewById(R.id.allalarms);
-        allalarms.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Welcome_Activity.this, MainActivityAlarms.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.settings:
+                        Intent intent = new Intent(Welcome_Activity.this, WelcomeActivity2.class);
+                        startActivity(intent);
+                        return true;
+
+                    case R.id.nav:
+                        Intent intent2 = new Intent(Welcome_Activity.this, Main2Activity.class);
+                        startActivity(intent2);
+                        return true;
+
+                    case R.id.create:
+                        clickCreate();
+                        return true;
+
+                    case R.id.exist:
+                        existing();
+                        return true;
+
+                    case R.id.allalarms:
+                        Intent intent3 = new Intent(Welcome_Activity.this, MainActivityAlarms.class);
+                        startActivity(intent3);
+                        return true;
+                    default:
+                        return false;
+                }
             }
         });
 
@@ -75,5 +77,13 @@ public class Welcome_Activity extends AppCompatActivity {
     public void AllAlarms(View view) {
         Intent intent = new Intent(this, MainActivityAlarms.class);
         this.startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
