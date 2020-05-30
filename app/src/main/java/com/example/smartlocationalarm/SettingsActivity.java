3b503a06a242fbbd0b5857d6ccaefb6f5bc15a6a
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-05-30 21:57:34 +0000

    navigDrawer on Settings

--- app/src/main/java/com/example/smartlocationalarm/SettingsActivity.java
+++ app/src/main/java/com/example/smartlocationalarm/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.example.smartlocationalarm;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,9 +14,16 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+import hotchemi.android.rate.AppRate;
 
 public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -26,10 +36,74 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
 
     SeekBar volControl;
 
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView nv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        AppRate.with(this)
+                .setInstallDays(0)
+                .setLaunchTimes(10)
+                .setRemindInterval(5)
+                .setShowLaterButton(true)
+                .monitor();
+        AppRate.showRateDialogIfMeetsConditions(this);
+        final Context c = this;
+        final Activity a = this;
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nv = findViewById(R.id.navView);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.create:
+                        clickCreate();
+                        return true;
+
+                    case R.id.exist:
+                        existing();
+                        return true;
+
+                    case R.id.allalarms:
+                        Intent intent3 = new Intent(SettingsActivity.this, alarmListActivity.class);
+                        startActivity(intent3);
+                        return true;
+                    case R.id.setting:
+                        Intent intent4 = new Intent(getApplicationContext(), SettingsActivity.class);
+                        startActivity(intent4);
+                        return true;
+                    case R.id.rating:
+                        AppRate.with(c).showRateDialog(a);
+                        return true;
+                    case R.id.share:
+                        Intent i = new Intent(Intent.ACTION_SEND);
+                        i.setType("text/plain");
+                        i.putExtra(Intent.EXTRA_SUBJECT, "Smart Location Alarm");
+                        String message = "\nJe voudrais vous recommander cette application Smart Location Alarm, prochainement nous allons la mettre dans le play store \n\n";
+
+                        i.putExtra(Intent.EXTRA_TEXT, message);
+                        startActivity(Intent.createChooser(i, "Share with others"));
+                        return true;
+                    case R.id.about:
+                        Intent intent5 = new Intent(getApplicationContext(), AboutActivity.class);
+                        startActivity(intent5);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
 
         spi = findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(SettingsActivity.this,
@@ -125,4 +199,23 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
     public void onNothingSelected(AdapterView<?> parent) {
         return;
     }
+
+    private void existing() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        this.startActivity(intent);
+    }
+
+    private void clickCreate() {
+        Intent intent = new Intent(this, PermissionActivity.class);
+        this.startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
