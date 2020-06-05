ca262e96878a782f5039bb9a69c615ba709293ed
AIT-RAMI <mohamedaitrami@gmail.com>
2020-06-05 01:56:33 +0100

    Alarm with no sound

--- app/src/main/java/com/example/smartlocationalarm/BackgroundLocationUpdateService.java
+++ app/src/main/java/com/example/smartlocationalarm/BackgroundLocationUpdateService.java
@@ -11,6 +11,8 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.location.Location;
 import android.location.LocationListener;
+import android.media.AudioAttributes;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -24,6 +26,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -67,6 +70,9 @@ public class BackgroundLocationUpdateService extends Service implements GoogleAp
     private LocationRequest mLocationRequest;
     private Location mCurrentLocation;
     /* For Google Fused API */
+    /* For notifications */
+    private static final String CHANNEL_ID = "MyNotificationChannelID";
+    private Ringtone ringtone;
 
     @Override
     public void onCreate() {
@@ -162,6 +168,38 @@ public class BackgroundLocationUpdateService extends Service implements GoogleAp
         } else {
             // Change those values for
             //load data
+            ringtone = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+            String CHANNEL_ID = "ff";
+            // Create an explicit intent for an Activity in your app
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+            final Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            final NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_alarms)
+                    .setContentTitle("Default name")
+                    .setContentText("Default notes")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    // Set the intent that will fire when the user taps the notification
+                    .setContentIntent(pendingIntent)
+                    .setSound(alarmSound)
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_SOUND);
+            ;
+
+            if (Build.VERSION.SDK_INT >= 26) {
+                NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "My Alarm clock Service", NotificationManager.IMPORTANCE_DEFAULT);
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(notificationChannel);
+                notificationChannel.enableVibration(true);
+                AudioAttributes attributes = new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                        .build();
+                notificationChannel.setSound(alarmSound, attributes);
+            }
+            final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+
             new firebaseDatabaseHelper(BackgroundLocationUpdateService.this).readAlarms(new firebaseDatabaseHelper.DataStatus() {
                 @Override
                 public void DataIsLoaded(List<alarm> alarms, List<String> keys) {
@@ -174,6 +212,17 @@ public class BackgroundLocationUpdateService extends Service implements GoogleAp
                         Boolean status = alarms.get(i).getStatus();
                         if (inRange(latDest, logDest, location.getLatitude(), location.getLongitude(), radius) && status) {
                             Log.d(TAG_LOCATION, "I'm in range! the alarm should notify :)");
+                            /* Notification Code*/
+                            int notificationId = 1111;
+                            // notificationId is a unique int for each notification that you must define
+                            builder.setContentTitle(Title);
+                            builder.setContentText(Note);
+                            Notification notification = builder.build();
+                            notification.defaults |= Notification.DEFAULT_SOUND;
+                            
+                            notificationManager.notify(notificationId, notification);
+
+                            /* end Notification Code */
                         }
                     }
                 }
